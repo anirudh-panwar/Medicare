93ec9981bc9b4b3073facf8609b2ddbb2ebc4d43
Anirudh Panwar <anirudhpanwar32@gmail.com>
2026-05-15 15:45:27 +0530

    added url

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const app = express();
 const port = 4000;
 
 const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
+    "https://medicare-frontend-6c7r.onrender.com",
+    "https://medicare-admin-rwdq.onrender.com",
 ];
 
 // Middlewwares
